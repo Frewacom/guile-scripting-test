811b594cb6f9c42b26fc43f281e4ab4a8419eb25
Fredrik Engstrand <fredrik@engstrand.nu>
2021-07-21 18:17:20 +0200

    feat: add monitor rules

--- config.scm
+++ config.scm
@@ -1,5 +1,8 @@
 (use-modules (gnu services configuration)
              (ice-9 match)
+             (ice-9 format)
+             (ice-9 pretty-print)
+             (ice-9 exceptions)
              (srfi srfi-1))
 
 ; Syntax helper to easily create callable actions.
@@ -21,29 +24,47 @@
 (define (procedure-or-bool? val) (or (procedure? val) (boolean? val)))
 (define (list-of-modifiers? lst) (every (lambda (x) (member x %modifiers)) lst))
 
-; Apply conditional transformations to singular
-; values inside the dwl configuration.
-(define (transform-value field value)
-  (match
-    field
-    ('keys (map (lambda (key) (transform-config <dwl-key> key)) value))
-    ('layouts (map (lambda (key) (transform-config <dwl-layout> key)) value))
-    ('rules (map (lambda (rule) (transform-config <dwl-rule> rule)) value))
-    (_ value)))
+; Layout configuration
+(define-configuration
+  dwl-layout
+  (id
+    (string)
+    "id that can be used to reference a layout, e.g. in a monitor rule")
+  (symbol
+    (string)
+    "symbol that should be shown when layout is active")
+  (arrange
+    (procedure-or-bool #f)
+    "procedure to call when selected")
+  (no-serialization))
 
-; Transforms a record into alist to allow the values to easily be
-; fetched via C using `scm_assoc_ref(alist, key)`.
-(define transform-config
-  (lambda (type config)
-    (remove
-      (lambda (pair) (equal? (car pair) "%location"))
-      (fold-right
-        (lambda (field acc)
-          (append
-            `((,(symbol->string field) . ,(transform-value field ((record-accessor type field) config))))
-            acc))
-        '()
-        (record-type-fields type)))))
+; Basic layouts
+(define %layout-default
+  (dwl-layout
+    (id "default")
+    (symbol "[]=")
+    (arrange
+      (dwl-procedure
+        (test-func "arrange 1")))))
+
+(define %layout-monocle
+  (dwl-layout
+    (id "monocle")
+    (symbol "[M]")
+    (arrange
+      (dwl-procedure
+        (test-func "arrange 1")))))
+
+(define %layout-floating
+  (dwl-layout
+    (id "floating")
+    (symbol "><>")))
+
+; Default layouts
+(define %base-layouts
+  (list %layout-default
+        %layout-monocle
+        %layout-floating))
 
 ; Application rule configuration
 (define-configuration
@@ -65,15 +86,24 @@
     "monitor to spawn application on")
   (no-serialization))
 
-; Layout configuration
+; Monitor rule configuration
 (define-configuration
-  dwl-layout
-  (symbol
-    (string)
-    "symbol that should be shown when layout is active")
-  (arrange
-    (procedure-or-bool #f)
-    "procedure to call when selected")
+  dwl-monitor-rule
+  (name
+    (string-or-bool #f)
+    "name of monitor, e.g. eDP-1")
+  (master-factor
+    (number 0.55)
+    "horizontal scaling factor for master windows")
+  (number-of-masters
+    (number 1)
+    "number of allowed windows in the master area")
+  (scale
+    (number 1)
+    "monitor scaling")
+  (layout
+    (string "default")
+    "default layout (id) to use for monitor")
   (no-serialization))
 
 ; Keybinding configuration
@@ -90,10 +120,16 @@
     "procedure to call when triggered")
   (no-serialization))
 
+; dwl configuration type predicates
 (define (list-of-tags? lst) (every string? lst))
 (define (list-of-keys? lst) (every dwl-key? lst))
 (define (list-of-rules? lst) (every dwl-rule? lst))
 (define (list-of-layouts? lst) (every dwl-layout? lst))
+(define (list-of-monitor-rules? lst) (every dwl-monitor-rule? lst))
+
+; Default monitor rules
+(define %base-monitor-rules
+  (list (dwl-monitor-rule)))
 
 ; dwl configuration
 (define-configuration
@@ -127,57 +163,149 @@
       (list "1" "2" "3" "4" "5" "6" "7" "8" "9"))
     "list of tag names")
   (layouts
-    (list-of-layouts '())
+    (list-of-layouts %base-layouts)
     "list of layouts to use")
   (rules
     (list-of-rules '())
     "list of application rules")
+  (monitor-rules
+    (list-of-monitor-rules %base-monitor-rules)
+    "list of monitor rules")
   (keys
     (list-of-keys '())
     "list of keybindings")
   (no-serialization))
 
-; Create and transform the configuration into
+; Custom dwl config
+(define dwl-config
+  (dwl-configuration
+    (border-px 2)
+    (tags
+      (list "1" "2" "3" "4" "5"))
+    (rules
+      (list
+        (dwl-rule
+          (id "firefox")
+          (tag 4))
+        (dwl-rule
+          (id "tidal")
+          (tag 5))))
+    (monitor-rules
+      (append
+        (list
+          (dwl-monitor-rule
+            (name "eDP-1")
+            (layout "monocle")))
+        %base-monitor-rules))
+    (keys
+      (list
+        (dwl-key
+          (modifiers
+            (list MODKEY MOD-SHIFT))
+          (key 1)
+          (action
+            (dwl-procedure
+              (test-func "action 1"))))
+        (dwl-key
+          (modifiers
+            (list MODKEY MOD-SHIFT))
+          (key 2))))))
+
+; Apply conditional transformations to singular
+; values inside the monitor rule configuration.
+(define (transform-monitor-rule field value original)
+  (match
+    field
+    ('layout
+     (let* ((layouts (dwl-configuration-layouts original))
+            (index (list-index (lambda (l) (equal? (dwl-layout-id l) value)) layouts)))
+       (match
+         index
+         (#f
+           (raise-exception
+             (make-exception-with-message
+               (string-append value " is not a valid layout id"))))
+         (_ index))))
+    (_ value)))
+
+; Apply conditional transformations to singular
+; values inside the root dwl configuration.
+(define (transform-config-value field value original)
+  (match
+    field
+    ('keys
+     (map
+       (lambda
+         (key)
+         (transform-config
+           #:transform-value transform-key-value
+           #:type <dwl-key>
+           #:config key
+           #:original-config original))
+       value))
+    ('layouts
+     (map
+       (lambda
+         (layout)
+         (transform-config
+           #:type <dwl-layout>
+           #:config layout
+           #:original-config original))
+       value))
+    ('rules
+     (map
+       (lambda
+         (rule)
+         (transform-config
+           #:type <dwl-rule>
+           #:config rule
+           #:original-config original))
+       value))
+    ('monitor-rules
+     (map
+       (lambda
+         (monitor-rule)
+         (transform-config
+           #:transform-value transform-monitor-rule
+           #:type <dwl-monitor-rule>
+           #:config monitor-rule
+           #:original-config original))
+       value))
+    (_ value)))
+
+; Apply conditional transformations to singular
+; values inside the keybinding configuration.
+(define (transform-key-value field value original)
+  (match
+    field
+    ('modifiers (delete-duplicates value))
+    (_ value)))
+
+; Transforms a record into alist to allow the values to easily be
+; fetched via C using `scm_assoc_ref(alist, key)`.
+(define*
+  (transform-config
+    #:key
+    (transform-value transform-config-value)
+    (type #f)
+    (config '())
+    (original-config '()))
+  (remove
+    (lambda (pair) (equal? (car pair) "%location"))
+    (fold-right
+      (lambda (field acc)
+        (append
+          (let ((accessor ((record-accessor type field) config)))
+          `((,(symbol->string field) . ,(transform-value field accessor original-config))))
+          acc))
+      '()
+      (record-type-fields type))))
+
+; Transform the configuration into
 ; a format that can be easily accessed from C.
 (define config
   (transform-config
-    <dwl-configuration>
-    (dwl-configuration
-      (border-px 2)
-      (tags
-        (list "1" "2" "3" "4" "5"))
-      (layouts
-        (list
-          (dwl-layout
-            (symbol "[]=")
-            (arrange
-              (dwl-procedure
-                (test-func "arrange 1"))))
-          (dwl-layout
-            (symbol "[M]")
-            (arrange
-              (dwl-procedure
-                (test-func "arrange 1"))))
-          (dwl-layout
-            (symbol "><>"))))
-      (keys
-        (list
-          (dwl-key
-            (modifiers
-              (list MODKEY MOD-SHIFT))
-            (key 1)
-            (action
-                (dwl-procedure
-                  (test-func "action 1"))))
-          (dwl-key
-            (modifiers
-              (list MODKEY MOD-SHIFT))
-            (key 2))))
-      (rules
-        (list
-          (dwl-rule
-            (id "firefox")
-            (tag 4))
-          (dwl-rule
-            (id "tidal")
-            (tag 5)))))))
+    #:transform-value transform-config-value
+    #:type <dwl-configuration>
+    #:config dwl-config
+    #:original-config dwl-config))
